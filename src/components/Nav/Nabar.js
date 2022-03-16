21a92ac15a1250103a738bd9b61da2a196205a70
Kakon Ray <thisiskakonray@gmail.com>
2022-03-17 01:00:03 +0600

    Navbar search api result show

--- src/components/Nav/Nabar.js
+++ src/components/Nav/Nabar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from "react";
 import {
   Navbar,
@@ -7,12 +8,12 @@ import {
   Form,
   FormControl,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 import SearchResult from "../SearchResult/SearchResult";
 import "./Navbar.css";
 
-export default function Nabar() {
+export default function Nabar(props) {
   const [show, setShow] = useState(false);
   const [searchValue, setInputValue] = useState("");
 
@@ -30,46 +31,60 @@ export default function Nabar() {
     setInputValue(e.target.value);
   };
 
+  const history = useHistory();
+  const location = useLocation();
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    history.push(`/search`);
+    if (location.pathname === "/search") {
+      props.func(searchValue);
+    }
+  };
   return (
     <>
       <Navbar sticky="top" bg="light" expand="lg" className={`navbar py-0`}>
         <Container>
           <div className="logo">
-            <a href="/" className="logo-style">
+            <Link
+              to="/"
+              style={{ textDecoration: "none" }}
+              className="logo-style"
+            >
               Sports
-            </a>
+            </Link>
           </div>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="ms-auto m-5 my-2 my-lg-0" navbarScroll>
               <Link to="/" style={{ textDecoration: "none" }}>
-                <Nav.Link href="/">Home</Nav.Link>
+                <a className="nav-link">Home</a>
               </Link>
               <Link to="/allsports" style={{ textDecoration: "none" }}>
-                <Nav.Link href="/allsports">All Sports</Nav.Link>
+                <a className="nav-link"> All Sports</a>
               </Link>
 
               <NavDropdown title="Other" id="navbarScrollingDropdown">
                 <Link to="/appointment" style={{ textDecoration: "none" }}>
-                  <Nav.Link href="/appointment">All Sports</Nav.Link>
+                  <a className="nav-link">All Sports</a>
                 </Link>
                 <Link to="/" style={{ textDecoration: "none" }}>
-                  <Nav.Link href="/">Home</Nav.Link>
+                  <a className="nav-link">Home</a>
                 </Link>
                 <Link to="/" style={{ textDecoration: "none" }}>
-                  <Nav.Link href="/">Appointment</Nav.Link>
+                  <a className="nav-link">Appointment</a>
                 </Link>
                 <Link to="/" style={{ textDecoration: "none" }}>
-                  <Nav.Link href="/">Contact</Nav.Link>
+                  <a className="nav-link">Contact</a>
                 </Link>
               </NavDropdown>
 
-              <Nav.Link href="/" disabled>
+              <Nav.Link to="/" disabled>
                 Actiion
               </Nav.Link>
 
               <Link to="/" style={{ textDecoration: "none" }}>
-                <Nav.Link href="/">Contact</Nav.Link>
+                <a className="nav-link">Contact</a>
               </Link>
             </Nav>
             <Form className="d-flex nav-form justify-content-center">
@@ -88,7 +103,8 @@ export default function Nabar() {
                 <button
                   id="formInput"
                   className="btn btn-outline-info me-2 mt-1"
-                  type="submit"
+                  // type="submit"
+                  onClick={handleSearch}
                 >
                   Search
                 </button>
@@ -102,7 +118,6 @@ export default function Nabar() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {searchValue ? <SearchResult value={searchValue} /> : null}
     </>
   );
 }
--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import CardContainer from "../Card/CardContainer";
+import Nabar from "../Nav/Nabar";
 
 export default function SearchResult({ value }) {
   const [users, setUser] = useState([]);
+  const [searchVlaue, setSearchvalue] = useState();
   useEffect(() => {
     fetch("https://www.thesportsdb.com/api/v1/json/2/all_sports.php")
       .then((respons) => respons.json())
@@ -13,17 +15,47 @@ export default function SearchResult({ value }) {
       });
   }, []);
 
+  const pull_data = (data) => {
+    setSearchvalue(data);
+  };
+
+  const usersNum = users?.slice(0, 25);
+  const userResult = users.filter(
+    (user) => user.strSport?.toLowerCase() === searchVlaue?.toLowerCase()
+  );
   return (
     <div className="container text-center mx-auto my-4">
-      {users.map((user) =>
-        user.strSport.toLowerCase() === value.toLowerCase() ? (
-          <CardContainer
-            img={user.strSportThumb}
-            title={user.strSport}
-            cardDes={user.strSportDescription.split("").slice(1, 100).join("")}
-          />
-        ) : null
-      )}
+      <Nabar func={pull_data} />
+      <div className="row mt-5">
+        {userResult.map((user) => (
+          <React.Fragment key={user.idSport}>
+            <div className="col-md-4">
+              <CardContainer
+                img={user.strSportThumb}
+                title={user.strSport}
+                cardDes={user.strSportDescription
+                  .split("")
+                  .slice(1, 100)
+                  .join("")}
+              />
+            </div>
+          </React.Fragment>
+        ))}
+        {usersNum.map((user) => (
+          <React.Fragment key={user.idSport}>
+            <div className="col-md-4">
+              <CardContainer
+                img={user.strSportThumb}
+                title={user.strSport}
+                cardDes={user.strSportDescription
+                  .split("")
+                  .slice(1, 100)
+                  .join("")}
+              />
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
